c48f7fbfa747736ebe0b4577cdcf2240e8a3ede7
Curso <hhector.agm@gmail.com>
2024-11-12 01:13:17 -0300

    subida apunte de logs

---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,13 @@
+package principal;
+
+import utilities.Logs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logs.error("Este es un mensaje de error");
+        Logs.info("Este es un mensaje de info");
+        Logs.debug("Este es un mensaje de debug");
+        Logs.trace("Este es un mensaje de trace");
+    }
+}
